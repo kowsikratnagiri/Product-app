45f52eccbdb5effb1568d7ea112d94a7e678fa32
kowsikratnagiri <kowsikratnagiri@gmail.com>
2025-06-06 00:43:04 +0530

    Config-repo added natively and configured config server for centralised configuration

--- gateway-service/src/main/java/com/service/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/service/gateway_service/GatewayServiceApplication.java
@@ -2,7 +2,6 @@ package com.service.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class GatewayServiceApplication {
 
